bb2704408fcf0ed980e73e951539f5023c70009a
China Wang <chinana523@gmail.com>
2016-07-28 08:58:50 -0700

    started strings

--- app/strings.js
+++ app/strings.js
@@ -6,10 +6,27 @@ exports.stringsAnswers = {
   },
 
   wordWrap: function(str, cols) {
+  	var words = str.split(' ');
+  	var i = 0;
+  	var currentLength = 0;
+  	var result = '';
 
+  	while (i < words.length) {
+  	  if (currentLength < cols) {
+  	  	result += words[i];
+  	  	currentLength += words[i].length;
+  	  } else {
+  	  	result += '\n' + words[i];
+  	  	currentLength = words[i].length;
+  	  }
+
+  	  i++;
+  	}
+
+  	return result;
   },
 
   reverseString: function(str) {
-
+  	return str.split('').reverse().join('');
   }
 };
