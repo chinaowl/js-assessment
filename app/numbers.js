60f713323d9d1c31b55322fb89db4e21400a9b2e
China Wang <chinana523@gmail.com>
2016-07-27 20:17:16 -0700

    finished numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,27 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+  	var bitArray = (num).toString(2).split('').reverse();
+  	return parseInt(bitArray[bit - 1], 10);
   },
 
   base10: function(str) {
-
+  	return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+  	var padding = '00000000';
+  	return (padding + (num).toString(2)).slice(-8);
   },
 
   multiply: function(a, b) {
+  	function getDecimals(n) {
+  	  var split = (n + '').split('.');
+  	  if (split.length !== 2) return 0;
+  	  return split[1].length;
+  	}
 
+  	var precision = Math.max(getDecimals(a), getDecimals(b));
+  	return +(a * b).toPrecision(precision);
   }
 };
