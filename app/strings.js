b9ac7855493228b02f8ee38ae481b08fccd11f7f
China Wang <chwang@thoughtworks.com>
2016-07-28 13:50:43 -0700

    finished strings

--- app/strings.js
+++ app/strings.js
@@ -2,27 +2,47 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    var result = '';
+    var previous, count = 0;
 
+    str.split('').forEach(function(character) {
+      if (character === previous) {
+        if (count < amount) {
+          result += character;
+        }
+        count++;
+      } else {
+        result += character;
+        count = 1;
+      }
+      
+      previous = character;
+    });
+
+    return result;
   },
 
   wordWrap: function(str, cols) {
   	var words = str.split(' ');
-  	var i = 0;
   	var currentLength = 0;
   	var result = '';
+    var word, wordLength;
 
-  	while (i < words.length) {
-  	  if (currentLength < cols) {
-  	  	result += words[i];
-  	  	currentLength += words[i].length;
-  	  } else {
-  	  	result += '\n' + words[i];
-  	  	currentLength = words[i].length;
-  	  }
+    for (var i = 0; i < words.length; i++) {
+      word = words[i];
+      wordLength = word.length;
 
-  	  i++;
-  	}
-
+      if (currentLength + wordLength + 1 < cols) {
+        if (i !== 0) result += ' ';
+        result += word;
+        currentLength += wordLength;
+      } else {
+        if (i !== 0) result += '\n';
+        result += word;
+        currentLength = wordLength;
+      }
+    }
+  	
   	return result;
   },
 
