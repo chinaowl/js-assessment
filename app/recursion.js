d50f41090c7c1c95157206a5584740286f766c63
China Wang <chwang@thoughtworks.com>
2016-07-28 16:56:00 -0700

    started recursion problems

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,25 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var files = [];
 
+    function listFilesHelper(data, foundDir) {
+      if (data.dir === dirName) foundDir = true;
+
+      data.files.forEach(function(item) {
+        if (typeof item === 'string') {
+          if (foundDir) {
+            files.push(item);
+          }
+        } else {
+          listFilesHelper(item, foundDir);
+        }
+      });
+    }
+
+    listFilesHelper(data, !dirName);
+
+    return files;
   },
 
   permute: function(arr) {
@@ -10,7 +28,13 @@ exports.recursionAnswers = {
   },
 
   fibonacci: function(n) {
+    function fibonacciHelper(n) {
+      if (n <= 2) return 1;
 
+      return fibonacciHelper(n - 1) + fibonacciHelper(n - 2);
+    }
+
+    return fibonacciHelper(n);
   },
 
   validParentheses: function(n) {
