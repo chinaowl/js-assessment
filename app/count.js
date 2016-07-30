65252dea59d2e508cefdd0abbbf32eb474da09e4
China Wang <chinana523@gmail.com>
2016-07-29 19:29:19 -0700

    finished counting

--- app/count.js
+++ app/count.js
@@ -1,7 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+  count: function(start, end) {
+    var timeout;
 
+    function logCount() {
+      console.log(start++);
+
+      if (start <= end) {
+        timeout = setTimeout(logCount, 100);
+      }
+    }
+
+    logCount();
+
+    return {
+      cancel: function() {
+        timeout && clearTimeout(timeout);
+      }
+    };
   }
 };
