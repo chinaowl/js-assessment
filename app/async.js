2473f8a5d7c9d0a58d54487310b04f2e6773efb3
China Wang <chinana523@gmail.com>
2016-07-29 19:57:33 -0700

    finished async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,32 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+  	return new Promise(function(resolve, reject) {
+  		resolve(value);
+  	});
   },
 
   manipulateRemoteData: function(url) {
+    var promise = new Promise(function(resolve, reject) {
+      var request = new XMLHttpRequest();
 
+      request.onload = function() {
+        if (this.status === 200) {
+          var response = JSON.parse(this.response).people;
+          response = response.map(function(item) {
+            return item.name;
+          }).sort();
+
+          resolve(response);
+        } else {
+          reject(this.statusText);
+        }
+      };
+
+      request.open('GET', url);
+      request.send();
+    });
+
+    return promise;
   }
 };
